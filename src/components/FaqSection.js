9003edc91d6e6dc5cdf71404a44301a3104d1e9d
Danny JeanLouis <dlouis3386@gmail.com>
2022-03-10 14:28:24 -0500

    add styling to faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,9 @@
+import { About as Layout } from "../styles";
+import styled from "styled-components";
+
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -13,6 +16,7 @@ const FaqSection = () => {
             repellendus.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products do you offer?</h4>
@@ -23,6 +27,7 @@ const FaqSection = () => {
             repellendus.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Differnt payment methods</h4>
@@ -43,9 +48,37 @@ const FaqSection = () => {
             repellendus.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(Layout)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
