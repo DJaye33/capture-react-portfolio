b5050fe9a74a2ab9e015b4ec44f6466633075833
Danny JeanLouis <dlouis3386@gmail.com>
2022-03-10 14:16:31 -0500

    add styling to service section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,10 @@
 import home1 from "../img/home1.png";
-import styled from "styled-components";
+// import styled from "styled-components";
+import {About as Layout, Description, Image, Hide} from "../styles"
 
 const AboutSection = () => {
   return (
-    <About>
+    <Layout>
       <Description>
         <div className="title">
           <Hide>
@@ -27,38 +28,9 @@ const AboutSection = () => {
       <Image className="image">
         <img src={home1} alt="man with a camera" />
       </Image>
-    </About>
+    </Layout>
   );
 };
 
-// Styled Component
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: #fff;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  font-weight: lighter;
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
